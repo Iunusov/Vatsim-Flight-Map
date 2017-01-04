12a0548ec831d99a0f48d4c42e595852b13ab561
Artur Iunusov <iunusov.artur@gmail.com>
2017-01-04 08:19:49 +0100

    various small fixes

--- js/App.js
+++ js/App.js
@@ -1,10 +1,9 @@
 "use strict";
 var App = function () {
-	var initialized = false;
 	var that = this;
 	var pTimeout = false;
-	var lastModified = null;
-	var map = null;
+	var lastModified = "";
+	var map = false;
 	var markersArray = [];
 	var clients = [];
 	var infowindow = null;
@@ -15,11 +14,6 @@ var App = function () {
 	var polyLine = null;
 	var clientTemplate = null;
 	var infowindowOpened = false;
-	var getProp = function (obj, prop) {
-		if (prop in obj)
-			return obj[prop];
-		return "";
-	}
 	var getFlightType = function (s) {
 		var result = s;
 		if (s === "I") {
@@ -30,36 +24,42 @@ var App = function () {
 		}
 		return result;
 	}
-	var formatDate = function (obj, prop) {
-		if (getProp(obj, prop) === "")
-			return "";
-		return getProp(obj, prop).substring(8, 10) + ":" + getProp(obj, prop).substring(10, 12) + ":" + getProp(obj, prop).substring(12, 14);
-	}
-	var formatNumber = function (num) {
-		var res = num.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-		if (res != "")
-			return res;
-		return "0";
-	}
-	var formatDepTime = function (obj, prop) {
-		var deptime = getProp(obj, prop);
-		if (deptime === "" || deptime === "0") {
-			return "";
+	//20170104045956 => 04:59:56
+	var formatDate = function (str) {
+		if (!str || str.length != 14 || !parseInt(str)) {
+			return str;
 		}
-		while (deptime.length < 4) {
-			deptime = "0" + deptime;
+		return str.substring(8, 10) + ":" + str.substring(10, 12) + ":" + str.substring(12, 14);
+	}
+	//34098 => 34,098
+	function commaSeparateNumber(val) {
+		if (!parseInt(val)) {
+			return val;
 		}
-		return deptime.substring(0, 2) + ":" + deptime.substring(2, 4);
+		while (/(\d+)(\d{3})/.test(val.toString())) {
+			val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
+		}
+		return val;
+	}
+	//520 => 05:20
+	var formatDepTime = function (str) {
+		if (!parseInt(str)) {
+			return str;
+		}
+		while (str.length < 4) {
+			str = "0" + str;
+		}
+		return str.substring(0, 2) + ":" + str.substring(2, 4);
 	}
 	var objectToHTML = function (client) {
-		client["altitude"] = formatNumber((getProp(client, "altitude")));
-		client["time_logon"] = formatDate(client, "time_logon");
+		client["altitude"] = commaSeparateNumber(client["altitude"]);
+		client["time_logon"] = formatDate(client["time_logon"]);
 		if (client.clienttype === "PILOT") {
-			client["planned_deptime"] = formatDepTime(client, "planned_deptime");
-			client["planned_actdeptime"] = formatDepTime(client, "planned_actdeptime");
+			client["planned_deptime"] = formatDepTime(client["planned_deptime"]);
+			client["planned_actdeptime"] = formatDepTime(client["planned_actdeptime"]);
 		} else
 			if (client.clienttype === "ATC") {
-				client["time_last_atis_received"] = formatDate(client, "time_last_atis_received");
+				client["time_last_atis_received"] = formatDate(client["time_last_atis_received"]);
 			}
 		return clientTemplate({
 			client : client
@@ -70,11 +70,11 @@ var App = function () {
 			polyLine.setMap(null);
 		}
 		infowindow.close();
-		infowindow.vs_cid = client.cid;
+		infowindow.vatsim_cid = client.cid;
 		infowindow.setContent(objectToHTML(client));
 		infowindow.open(map, marker);
 		infowindowOpened = true;
-		if (client.clienttype === "PILOT" && "planned_destairport_lat" in client && client.planned_destairport_lat != 0 && "planned_destairport_lon" in client && client.planned_destairport_lon != 0 && "planned_depairport_lat" in client && client.planned_depairport_lat != 0 && "planned_depairport_lon" in client && client.planned_depairport_lon != 0)
+		if (client.clienttype === "PILOT" && parseFloat(client["planned_destairport_lat"]) && parseFloat(client["planned_destairport_lon"]) && parseFloat(client["planned_depairport_lat"]) && parseFloat(client["planned_depairport_lon"])) {
 			polyLine = new google.maps.Polyline({
 					path : [new google.maps.LatLng(client.planned_depairport_lat, client.planned_depairport_lon), new google.maps.LatLng(client.latitude, client.longitude), new google.maps.LatLng(client.planned_destairport_lat, client.planned_destairport_lon), ],
 					strokeColor : "#FF0000",
@@ -83,6 +83,7 @@ var App = function () {
 					geodesic : true,
 					map : map
 				});
+		}
 		that.onOpenInfoWindow(client);
 	};
 	var requestClientDetails = function (cid, callsign, cb) {
@@ -109,17 +110,13 @@ var App = function () {
 		if (polyLine) {
 			polyLine.setMap(null);
 		}
-		infowindow.vs_cid = -1;
+		infowindow.vatsim_cid = -1;
 		for (var i = 0; i < tmpMarkersArray.length; i++) {
 			tmpMarkersArray[i].setMap(null);
-			delete tmpMarkersArray[i].vatsim_client_arr;
 		}
 		tmpMarkersArray = [];
 		that.onCloseInfoWindow();
 		infowindowOpened = false;
-	}
-	this.isInitialized = function () {
-		return initialized;
 	}
 	this.getMap = function () {
 		return map;
@@ -140,10 +137,6 @@ var App = function () {
 	}
 	this.getClientsFromServer = function () {
 		var dfd = $.Deferred();
-		if (!initialized) {
-			dfd.reject();
-			return dfd.promise();
-		}
 		$.ajax({
 			type : "GET",
 			url : "getclients.php",
@@ -151,17 +144,16 @@ var App = function () {
 			dataType : "json",
 			success : function (data, textStatus, request) {
 				var lm = request.getResponseHeader('Last-Modified');
-				if (lm != null && lm == lastModified) {
+				if (lm && lm == lastModified) {
 					dfd.resolve();
 					return dfd.promise();
 				}
 				lastModified = lm;
 				for (var i = 0; i < markersArray.length; i++) {
-					if (infowindow.vs_cid === markersArray[i].vatsim_client_arr[0]) {
+					if (infowindow.vatsim_cid === markersArray[i].vatsim_cid) {
 						tmpMarkersArray.push(markersArray[i]);
 					} else {
 						markersArray[i].setMap(null);
-						delete markersArray[i].vatsim_client_arr;
 					}
 					google.maps.event.clearInstanceListeners(markersArray[i]);
 				}
@@ -191,7 +183,8 @@ var App = function () {
 								map : map,
 								title : callsign,
 								icon : icon,
-								vatsim_client_arr : clients[index]
+								vatsim_cid : client[0],
+								vatsim_callsign : client[1]
 							})
 							marker.setMap(map);
 						google.maps.event.addListener(marker, 'click', function () {
@@ -219,17 +212,9 @@ var App = function () {
 	}
 	this.initialize = function (conf) {
 		clientTemplate = _.template(require("raw-loader!../tpl/details.html"));
-		defaultLocation = new google.maps.LatLng(44.996883999209636, -18.800782187499979);
-		mapTypeId = google.maps.MapTypeId.TERRAIN;
-		if (conf['map_center_lat'] && conf['map_center_lng']) {
-			defaultLocation = new google.maps.LatLng(conf['map_center_lat'], conf['map_center_lng']);
-		}
-		if (conf['map_zoom']) {
-			zoom = parseInt(conf['map_zoom']);
-		}
-		if (conf['map_type']) {
-			mapTypeId = conf['map_type'];
-		}
+		zoom = parseInt(conf['map_zoom']) || zoom;
+		defaultLocation = new google.maps.LatLng(parseFloat(conf['map_center_lat']) || 44.996883999209636, parseFloat(conf['map_center_lng']) || -18.800782187499979);
+		mapTypeId = _.contains(google.maps.MapTypeId, conf['map_type']) ? conf['map_type'] : google.maps.MapTypeId.TERRAIN;
 		infowindow = new google.maps.InfoWindow({});
 		map = new google.maps.Map(document.getElementById("map_canvas"), {
 				zoom : zoom,
@@ -254,16 +239,12 @@ var App = function () {
 			__onCloseInfoWindow();
 			infowindow.close();
 		});
-		initialized = true;
 	};
 	this.searchForCallsign = function (callsign) {
-		if (!initialized) {
-			return;
-		}
 		callsign = $.trim(callsign.toUpperCase());
 		for (var i = 0; i < markersArray.length; i++) {
-			var current_calsign = markersArray[i].vatsim_client_arr[1];
-			var current_cid = markersArray[i].vatsim_client_arr[0];
+			var current_calsign = markersArray[i].vatsim_callsign;
+			var current_cid = markersArray[i].vatsim_cid;
 			if (current_calsign === callsign) {
 				requestClientDetails(current_cid, callsign, function (clientDetails) {
 					openInfoWindow(clientDetails, map, markersArray[i]);
--- js/Callbacks.js
+++ js/Callbacks.js
@@ -1,43 +1,59 @@
 "use strict";
 var Callbacks = function () {
-	var infoWindowContentId = "#infoWindowContent";
-	var searchrow = $("#searchrow");
-	var inputCallsign = $("#inputCallsign");
-	var buttonCallsign = $("#buttonCallsign");
-	var saveParamsToLocalStorage = function (app) {
-		if (("localStorage" in window) && window["localStorage"] != null && app.isInitialized()) {
+	var app = {};
+
+	this.setApp = function (app_) {
+		app = app_;
+	}
+
+	this.start = function () {
+		var conf = {};
+		if (("localStorage" in window) && window["localStorage"] != null) {
+			conf["map_center_lat"] = localStorage.getItem("map_center_lat");
+			conf["map_center_lng"] = localStorage.getItem("map_center_lng");
+			conf["map_zoom"] = localStorage.getItem("map_zoom");
+			conf["map_type"] = localStorage.getItem("map_type");
+		}
+		app.initialize(conf);
+		bindHandlers();
+		app.doPoll().then(function () {
+			var callSign = app.getUrlParam("c", document.URL);
+			if (callSign) {
+				app.searchForCallsign(callSign);
+			}
+		});
+	}
+
+	var saveParamsToLocalStorage = function (app, callSign) {
+		if (("localStorage" in window) && window["localStorage"] != null && app.getMap()) {
 			localStorage.setItem("map_center_lat", app.getMap().getCenter().lat());
 			localStorage.setItem("map_center_lng", app.getMap().getCenter().lng());
 			localStorage.setItem("map_zoom", app.getMap().zoom);
 			localStorage.setItem("map_type", app.getMap().getMapTypeId());
+			localStorage.setItem("currentCallsign", callSign);
 		}
-		localStorage.setItem("currentCallsign", inputCallsign.val());
 	}
-	this.set = function (app) {
-		window.googleMapsLoaded = function () {
-			var conf = {};
-			if (("localStorage" in window) && window["localStorage"] != null) {
-				conf["map_center_lat"] = localStorage.getItem("map_center_lat");
-				conf["map_center_lng"] = localStorage.getItem("map_center_lng");
-				conf["map_zoom"] = parseInt(localStorage.getItem("map_zoom"));
-				conf["map_type"] = localStorage.getItem("map_type");
-				var currentCallsign = localStorage.getItem("currentCallsign");
-				if (currentCallsign) {
-					inputCallsign.val(currentCallsign);
-				}
+
+	var getCallsignFromLocalStorage = function () {
+		if (("localStorage" in window) && window["localStorage"] != null) {
+			var currentCallsign = localStorage.getItem("currentCallsign");
+			if (currentCallsign) {
+				return currentCallsign;
 			}
-			app.initialize(conf);
-			app.doPoll().then(function () {
-				searchrow.removeClass("hidden");
-				var callSign = app.getUrlParam("c", document.URL);
-				if (callSign) {
-					app.searchForCallsign(callSign);
-				}
-			});
-		};
-		$(window).focus(function () {
-			app.doPoll();
-		});
+		}
+		return "";
+	}
+
+	var bindHandlers = function () {
+		var infoWindowContentId = "#infoWindowContent";
+		var searchrow = $("#searchrow");
+		var inputCallsign = $("#inputCallsign");
+		var buttonCallsign = $("#buttonCallsign");
+
+		searchrow.removeClass("hidden");
+
+		inputCallsign.val(getCallsignFromLocalStorage());
+
 		app.onOpenInfoWindow = function (client) {
 			searchrow.hide();
 			$(infoWindowContentId).parent().parent().css("max-height", "9999px");
@@ -47,20 +63,23 @@ var Callbacks = function () {
 					history.replaceState({}, document.title, "?c=" + callSign);
 				}
 			}
-			saveParamsToLocalStorage(app);
+			saveParamsToLocalStorage(app, inputCallsign.val());
 		}
+
 		app.onCloseInfoWindow = function () {
 			searchrow.show();
 			if (window && window.history && window.history.replaceState) {
 				history.replaceState({}, document.title, '//' + location.host + location.pathname);
 			}
-			saveParamsToLocalStorage(app);
+			saveParamsToLocalStorage(app, inputCallsign.val());
 		}
+
 		app.onReceiveClientsArray = function (callSignsArray) {
 			inputCallsign.autocomplete("option", {
 				source : callSignsArray
 			});
 		}
+
 		inputCallsign.autocomplete({
 			source : [],
 			open : function (result) {
@@ -75,20 +94,27 @@ var Callbacks = function () {
 				}, 200);
 			}
 		});
+
 		buttonCallsign.click(function () {
 			setTimeout(function () {
 				app.searchForCallsign(inputCallsign.val());
 			}, 200);
 		});
+
 		inputCallsign.keypress(function (e) {
 			if (e.which == 13) {
 				buttonCallsign.click();
 				return false;
 			}
 		});
+
 		window.onbeforeunload = function (e) {
-			saveParamsToLocalStorage(app);
+			saveParamsToLocalStorage(app, inputCallsign.val());
 		};
+
+		$(window).focus(function () {
+			app.doPoll();
+		});
 	}
 }
 module.exports = Callbacks;
