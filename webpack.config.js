7620400c007b4ea35a00975aa59cee802418ca4d
Artur Iunusov <iunusov.artur@gmail.com>
2017-01-07 02:38:40 +0300

    async

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 module.exports = {
 	module : {
 		loaders : [{
@@ -21,15 +22,14 @@ module.exports = {
 		]
 	},
 	entry : {
-		app : ['./prod.js', './js/main.js'],
-		vendor : ['jquery', 'jquery-ui/ui/widgets/autocomplete', 'underscore', 'bootstrap', 'bootstrap/dist/css/bootstrap.css', 'jquery-ui/themes/base/autocomplete.css', 'jquery-ui/themes/base/core.css', 'jquery-ui/themes/base/menu.css', 'jquery-ui/themes/base/theme.css']
+		app : ['jquery', 'jquery-ui/ui/widgets/autocomplete', 'underscore', 'bootstrap', 'bootstrap/dist/css/bootstrap.css', 'jquery-ui/themes/base/autocomplete.css', 'jquery-ui/themes/base/core.css', 'jquery-ui/themes/base/menu.css', 'jquery-ui/themes/base/theme.css', './prod.js', './js/main.js']
 	},
 	output : {
 		path : 'dist/',
 		filename : '[name]_[chunkhash].js',
 		publicPath : 'dist/'
 	},
-	plugins : [new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor_[chunkhash].js'), new webpack.ProvidePlugin({
+	plugins : [new webpack.ProvidePlugin({
 			$ : 'jquery',
 			jQuery : 'jquery',
 			'window.jQuery' : 'jquery',
@@ -38,5 +38,7 @@ module.exports = {
 			template : './tpl/index.html',
 			filename : '../index.html',
 			inject : 'body',
-		}), ],
+		}), new ScriptExtHtmlWebpackPlugin({
+			defaultAttribute : 'async'
+		})],
 };
