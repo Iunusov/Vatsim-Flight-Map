2bbfe2b7f495978f73f45a7c5ba3205a555241dd
Artur Iunusov <aaiunusov@gmail.com>
2020-04-11 03:47:36 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var Callbacks = require("./Callbacks.js");
 $(document).ready(function () {
     if (!window.MAPS_API_KEY || window.MAPS_API_KEY === "your_key_here") {
-        $('#map').html('<div class="container"><br><br><br><br><h1>MapBox API key</h1><p>Please obtain your API key from the site: <a href="https://docs.mapbox.com/help/glossary/access-token/">https://docs.mapbox.com/help/glossary/access-token/</a></p><p>Then put it into <strong>prod.js</strong> file</p><p><a class="btn btn-primary btn-lg" href="https://docs.mapbox.com/help/glossary/access-token/" role="button">Get a Key &raquo;</a></p></div>');
+        $('#map').html('<div class="container"><br><br><br><br><h1>MapBox API key</h1><p>Please obtain your API key from the site: <a href="https://docs.mapbox.com/help/glossary/access-token/">https://docs.mapbox.com/help/glossary/access-token/</a></p><p>Then put it into <strong>prod.js</strong> file (!!!and then rebuild using webpack command from Readme!!!)</p><p><a class="btn btn-primary btn-lg" href="https://docs.mapbox.com/help/glossary/access-token/" role="button">Get a Key &raquo;</a></p></div>');
     } else {
         window.mapboxgl.accessToken = MAPS_API_KEY;
 		new Callbacks().start();
