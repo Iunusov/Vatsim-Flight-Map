5f54bb2769901a54e7e1ccfe9258b982754f7cd6
Artur Iunusov <aaiunusov@gmail.com>
2020-11-03 02:31:00 +0100

    Cache-Control

--- api/http.php
+++ api/http.php
@@ -3,21 +3,17 @@ namespace http;
 
 require_once ('../config.php');
 
-function write404($error = '{"error": "not found"}')
-{
-    http_response_code(404);
-    header('Last-Modified: ' . gmdate('D, d M Y H:i:s \G\M\T'));
-    header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + CACHE_LIFETIME_SECONDS));
-    header('Content-type: application/json');
-    header('Content-length: ' . strlen($error));
-    die($error);
-}
-
 function write($json)
 {
-    if (!strlen($json)) write404();
+    if (empty($json))
+    {
+        http_response_code(404);
+        $json = '{"error": "not found"}';
+    }
     header('Last-Modified: ' . gmdate('D, d M Y H:i:s \G\M\T'));
     header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + CACHE_LIFETIME_SECONDS));
+    header("Pragma: cache");
+    header("Cache-Control: max-age=" . CACHE_LIFETIME_SECONDS);
     header('Content-type: application/json');
     header('Content-length: ' . strlen($json));
     die($json);
